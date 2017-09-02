e6ccad0ddb99c8d96715a8328d6182ddb034704b
code1031q <dutingting@qq.com>
2017-09-03 00:12:41 +0800

    添加富文本

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language','uploadify','region'],function($,template,util){
+define(['jquery','template','util','ckeditor','datepicker','language','uploadify','region'],function($,template,util,CKEDITOR){
 	//设置导航菜单选中
 	util.setMenu('/main/index');
 	//调后台接口获取所有个人信息
@@ -32,6 +32,13 @@ define(['jquery','template','util','datepicker','language','uploadify','region']
 		    $('#pcd').region({
 		        url : '/public/assets/jquery-region/region.json'
 		    });
+		    // 处理富文本
+      		CKEDITOR.replace('editor',{
+        	toolbarGroups : [
+	          { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+	          { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
+	        ]
+	      });
 		}
 	});
 });
